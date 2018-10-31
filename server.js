e05fd3e18bb2d4f45cbae32df1918f5a473f937d
Web-Usov <web.usov@ya.ru>
2018-10-31 22:01:36 +0400

    up server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const app = require('express').express();
-const http = require('http');
+require('dotenv').config()
+var express = require('express');
+var http = require('http');
 
+const app = express();
 const server = http.Server(app);
 const port = process.env.PORT || 3000;
 
