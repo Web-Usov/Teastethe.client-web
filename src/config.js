0d0a58a5705e821bc247429cb5b2bb398e211ec2
Web-Usov <web.usov@ya.ru>
2018-11-07 21:30:12 +0400

    set config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const SERVER = "http://localhost:3100"
+// export const SERVER = "http://localhost:3100"
 // export const SERVER = "http://104.211.35.221:3100"
+export const SERVER = "http://13.95.82.226:3000" // chobzacodit
